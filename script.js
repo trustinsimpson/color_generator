82b9f86471fa82076ebc1b5df1b86912c671e27c
trustinsimpson <68867028+trustinsimpson@users.noreply.github.com>
2020-08-23 22:09:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+﻿let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let button = document.getElementById("b1");
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+function changeBackground() {
+	body.style.background =
+		"linear-gradient(to right, "
+		+ color1.value
+		+ ", "
+		+ color2.value
+		+ ")";
 
 	css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
+	const randomBg = () => {
+		const randomColor1 = Math.floor(Math.random() * 16777215).toString(16);
+		const randomColor2 = Math.floor(Math.random() * 16777215).toString(16);
+		color1.value = "#" + randomColor1;
+		color2.value = "#" + randomColor2;
+		console.log(color1);
+		console.log(color2);
+		changeBackground();
+	}
 
-color2.addEventListener("input", setGradient);
+color1.addEventListener("input", changeBackground);
+
+color2.addEventListener("input", changeBackground);
+
+button.addEventListener("click", randomBg);
